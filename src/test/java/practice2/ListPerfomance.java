bd5a366b3d1fdfc280ab52e6cd163593347e4196
maxloskut <lem123squizy@gmail.com>
2022-02-24 14:38:01 +0200

    __

--- src/test/java/practice2/ListPerfomance.java
+++ src/test/java/practice2/ListPerfomance.java
@@ -1,40 +1,39 @@
 package practice2;
 
-
 import java.util.*;
 
 public class ListPerfomance {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         //4) Check performance for some List operations (get(i),
         // insertAfter(i), ..) for different collection implementations.
 
         List<String> list1 = new ArrayList<>();
         List<String> list2 = new LinkedList<>();
-        for(int i = 0; i < 1000; i++){
+
+        for(int i = 0; i < 1000;i++){
             list1.add(UUID.randomUUID().toString());
             list2.add(UUID.randomUUID().toString());
-
-            System.out.println("Get Array: " + checkPerfGet(list1));
-            System.out.println("Get Linked: " + checkPerfGet(list2));
-
-            System.out.println("Set Array: " + checkPerfSet(list1));
-            System.out.println("Set Linked: " + checkPerfSet(list2));
         }
+        System.out.println("GET Array: "+checkPerfGet(list1));
+        System.out.println("GET Linked: "+checkPerfGet(list1));
+
+        System.out.println("SORT Array: "+checkPerfGet(list2));
+        System.out.println("SORT Linked: "+checkPerfGet(list2));
     }
 
     private static long checkPerfGet(List<String> list1) {
         long start = new Date().getTime();
-        Random random = new Random();
-        for(int i = 0; i< 10000; i++){
-            int length =
-            list1.get(random.nextInt(list1.size() - 1)).length();
-        }
-        return new Date().getTime() - start;
-    }
+        Random rand = new Random();
 
-    private static long checkPerfSet(List<String> list) {
+        for (int i = 0; i<1000; i++){
+            int length =
+                    list1.get(rand.nextInt( list1.size()-1)).length();
+        }
+        return new Date().getTime()-start;
+    }
+    private static long checkSort(List<String> list) {
         long start = new Date().getTime();
-        for(int i = 0; i< 100000; i++){
+        for (int i = 0; i < 100000; i++){
             list.add(UUID.randomUUID().toString());
         }
         return new Date().getTime() - start;
